2f7040006d92ec7c915bbb2a3d6926f838945563
vovanolo <vpankiv@softserveinc.com>
2023-11-13 19:39:03 +0200

    lesson1

---
+++ app.js
@@ -0,0 +1,107 @@
+// console.log("hello world!");
+
+// console.log("hello guys");
+
+// console.log("hello team");
+
+// (let const) var
+
+// let message; // created variable
+
+// message = "helo helo";
+
+// message = "hello man";
+
+
+// console.log(message);
+
+// node js
+
+// const message = "hello world!";
+
+// message = "hello man!";
+
+
+// let message = "hello man!";
+
+// let newMessage = "hello world!";
+
+// message = newMessage;
+
+// console.log(message);
+// console.log(newMessage);
+
+// let number = 5;
+
+// number = "hello";
+
+// console.log(number);
+
+// 232323
+// -2323232
+// 2.323
+
+// const number = 2323;
+
+// string = text
+
+// const message = "hello 'guys' 3435#$#$35 // 2242424"
+
+// boolean
+
+// true false
+
+// let data = true;
+
+// 
+
+// let message;
+
+// console.log(message);
+
+// object
+
+// const user = {
+//     name: "Ivan",
+//     age: 5,
+//     isAlive: true,
+//     info: undefined
+// }
+
+
+// let x = 5;
+
+// console.log(typeof x);
+
+// let y = "ssfsf";
+
+// y = true;
+
+// console.log(typeof(y));
+
+
+// взаємодія з користувачем
+
+// alert() 
+// prompt()
+// confirm()
+
+// alert(55);
+
+
+// let name = prompt("what is your name", "Volodymyr");
+
+// alert("Hello your name is " + name);
+
+// alert(`Hello your name is ${name}`);
+ 
+// let isOk = confirm("Are you ok?");
+
+// console.log(isOk);
+
+// * / + -
+
+// let x = 10;
+// let y = 100;
+
+// console.log(x * y);
