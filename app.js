a9439be035441986c649baca04e905ca9f00f3d1
vovanolo <vpankiv@softserveinc.com>
2023-11-15 19:58:07 +0200

    lesson2

--- app.js
+++ app.js
@@ -105,3 +105,123 @@
 // let y = 100;
 
 // console.log(x * y);
+
+// **
+
+// Math
+
+// Math.pow(2,3);
+
+// Math.sqrt(16);
+
+
+// true false
+
+// console.log(100 == 100);
+// console.log(100 == 101);
+
+// false - 0 ""
+// true - 1 "sf"
+
+// console.log("10" + "5"); // "105"
+
+// let age = prompt("how old are you?");
+
+// let result = (age > 18) ? "you adult" : "you not adult";
+
+// console.log(result);
+
+// const people = [];
+
+// const people = ["Ivan", "Petro", "Mykhailo", "Diana", 55, true, null];
+// const people = ["Ivan" - 0, "Petro" - 1, "Mykhailo" - 2, "Diana" - 3];
+const people = ["Ivan", "Petro", "Mykhailo", "Diana"];
+
+
+
+// console.log(people[0]);
+// people[2] = "John";
+// console.log(people[2]);
+// people[4] = "Peter";
+
+// people[8] = "Volodymyr";
+
+// console.log(people[7]);
+
+// довжина масиву = кількість елементів масиву
+// console.log(people.length);
+
+// people.push("Peter", "John");
+
+// people.pop();
+
+// console.log(people);
+
+let age = +prompt("How old are you?");
+
+// if (age > 18){
+//     console.log("you adult");
+// }
+
+// if (age > 18){
+//     console.log("you adult");
+// } else {
+//     console.log("you not adult");
+// }
+
+// if (age > 18 && age < 30){
+//     console.log("you are young age");
+// } else if(age > 30 && age < 40){
+//     console.log("you are middle age");
+// } else if(age > 40 && age < 50){
+//     console.log("you are really adult");
+// } else {
+//     console.log("wrong age");
+// }
+
+// switch case
+
+
+// switch(true){
+//     case age > 18 && age < 30: // true === age > 18 && age < 30
+//         console.log("you are young age");
+//         break;
+//     case age > 30 && age < 40:
+//         console.log("you are middle age");
+//         break;
+//     case age > 40 && age < 50:
+//         console.log("you are really adult");
+//         break;
+// }
+
+// switch(age){
+//     case 18:  age === 18
+//         console.log("you are 18");
+//         break;
+//     case 20:
+//         console.log("you are 20");
+//         break;
+//     default:
+//         console.log("you are not 18 and not 20");
+
+// }
+
+// if(age === 18){
+//     console.log("you are 18");
+// }else if(age === 20){
+//     console.log("you are 20");
+// }else{
+//     console.log("you are not 18 and not 20");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
